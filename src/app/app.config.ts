28615d9145208d13545db6dbd26aade8bb683054
vikaspulluri <vikasiiitn@gmail.com>
2018-08-12 21:03:47 +0530

    Transpiled code

--- src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-    'hostUrl': 'https://anapioficeandfire.com/api',
-    'errorMsgs':{
-        'validationErrors':{
-            'atleastOneRequired': 'Atleast one filter should be selected!!!'
-        }
-    }
-};
